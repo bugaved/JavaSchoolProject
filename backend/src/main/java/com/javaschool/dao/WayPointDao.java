068d140f2ad066f4ff1ded169554801b9743794e
Viacheslav Rostov <bugaved@gmail.com>
2017-10-15 21:22:32 +0300

    adds buying page and schedule

--- backend/src/main/java/com/javaschool/dao/WayPointDao.java
+++ backend/src/main/java/com/javaschool/dao/WayPointDao.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 @Component
-public class WaypointDao extends AbstractDao<Waypoint> {
+public class WayPointDao extends AbstractDao<Waypoint> {
     /**
      * {@inheritDoc}
      */
